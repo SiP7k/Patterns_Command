4f4380d82cf4f3e747f37067b5a46db24ead05a0
Савва Чальцев <cavvach@gmail.com>
2023-03-20 18:14:44 -0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Patterns_Command
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
